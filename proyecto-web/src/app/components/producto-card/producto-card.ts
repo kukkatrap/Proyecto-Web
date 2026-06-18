1f4d41a60ca305e62b315b7e930cfb4d8aa769b4
Lucas Ferreira <lucasnicoferreira@gmail.com>
2026-06-18 14:39:43 -0300

    Add files via upload

---
+++ proyecto-web/src/app/components/producto-card/producto-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-producto-card',
+  imports: [],
+  templateUrl: './producto-card.html',
+  styleUrl: './producto-card.css',
+})
+export class ProductoCard {}
